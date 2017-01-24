a9e844fe0cc576c3ead0a76709bbff8d724ae660
Kimberly Lalmansingh <kimlalman28@gmail.com>
2017-01-24 15:26:09 -0500

    Fixed onclick scroll for nav bar

--- public/script.js
+++ public/script.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
-	$("#plink").on("click", function(e){
+	$(".plink").on("click", function(e){
 		e.preventDefault();
 			if(this.hash !== ""){
 				var hash = this.hash;
-				$('html, #portfoliobody').animate({
+				$('html, body').animate({
 					scrollTop: $(hash).offset().top
-				}, 3000, function(){
+				}, 2000, function(){
 					window.location.hash = hash;
 				})
 			}
 	})
 })
+
+
